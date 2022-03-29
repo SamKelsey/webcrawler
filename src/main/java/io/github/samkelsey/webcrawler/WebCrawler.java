151ed52fb953cfa7749c9484699521fca0f20371
Sam Kelsey <sam_kelsey@hotmail.co.uk>
2022-03-29 20:53:43 +0100

    :boom: Initial repo skeleton.

---
+++ src/main/java/io/github/samkelsey/webcrawler/WebCrawler.java
@@ -0,0 +1,13 @@
+package io.github.samkelsey.webcrawler;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WebCrawler {
+
+    private final Logger logger = LoggerFactory.getLogger(WebCrawler.class);
+
+    public void run() {
+        logger.info("Running webcrawler");
+    }
+}
---
+++ src/test/java/io/github/samkelsey/webcrawler/WebCrawlerTest.java
@@ -0,0 +1,13 @@
+package io.github.samkelsey.webcrawler;
+
+import org.junit.jupiter.api.Test;
+
+public class WebCrawlerTest {
+
+    @Test
+    void example() {
+        WebCrawler webCrawler = new WebCrawler();
+        webCrawler.run();
+    }
+
+}
